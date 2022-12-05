5bf747fe388a0ec7cb5cc43fcb786b27abb32bd0
W-Carlos <wendcarlos18@hotmail.com>
2022-12-05 14:57:37 -0300

    Adding new item to list

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,36 @@
-import { v4 as uuidv4 } from 'uuid'
+import React, { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 
 function App() {
-  const list = [
-    {id: uuidv4(), task:"Estudar progrmação"},
-    {id: uuidv4(), task:"Comprar pão"}
-  ]
+  const [list, setList] = useState([]) // Estado que guarda a lista completa
+  const [inputTask, setInputTask] = useState('') // Estado que guarda o valor do input
 
-  console.log(list)
-
+  // Função para salvar o valor digitado no input
   function inputChange(event) {
-    console.log(event.target.value)
+    setInputTask(event.target.value)
+  }
+
+  // Função para adicionar o valor digitado na lista
+  function addTask() {
+    setList([ ...list, { id: uuidv4(), task: inputTask } ])
   }
 
   return (
     <div>
-      <input onChange={inputChange} type="text" placeholder="O que tenho que fazer..." />
-      <button>Adicionar</button>
-      
+      <input
+        onChange={inputChange}
+        type="text"
+        placeholder="O que tenho que fazer..."
+      />
+      <button onClick={addTask}>Adicionar</button>
+
       <ul>
-        {
-          list.map( (listItem) => {
-            return <li key={listItem.id}>{listItem.task}</li>
-          })
-        }
+        {list.map((listItem) => {
+          return <li key={listItem.id}>{listItem.task}</li>;
+        })}
       </ul>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
